e7926b1e278fae482179f78f9b61d4a1ba87a031
Chris Berthiaume <chrisbee@uw.edu>
2019-08-07 14:08:25 -0700

    Automate build product zip file creation

--- pyinstaller/build_macos.sh
+++ pyinstaller/build_macos.sh
@@ -2,3 +2,8 @@
 git clean -fdx hooks macos
 cd macos || exit
 ./build.sh
+
+# Zip pyinstaller executable to keep executable permissions
+cd dist || exit
+mkdir seaflowpy-$(git describe)-darwin && cp seaflowpy seaflowpy-$(git describe)-darwin/seaflowpy-darwin
+zip -0 seaflowpy-$(git describe)-darwin.zip -r seaflowpy-$(git describe)-darwin
