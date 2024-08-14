5423103a5cca53609568d1dd90c4aa738f58551a
Chris T. Berthiaume <chrisbee@uw.edu>
2024-08-14 11:12:08 -0700

    Refactor vct curate

--- src/seaflowpy/cli/commands/vct_cmd.py
+++ src/seaflowpy/cli/commands/vct_cmd.py
@@ -18,18 +18,18 @@ def vct_cmd():
 
 @vct_cmd.command("curate")
 @click.option("-d", "--db", "dbpath",
-    required=True, metavar="FILE", type=click.Path(dir_okay=False, readable=True),
+    metavar="FILE", type=click.Path(dir_okay=False, readable=True),
     help="Popcycle SQLite3 db file with populated outlier and sfl tables.")
-@click.option("--outdir",
+@click.option("-o", "--outdir",
     type=click.Path(path_type=pathlib.Path, file_okay=False, writable=True), required=True,
     help="""Output path for parquet file with subsampled event data.""")
 @click.option("-p", "--process-count", default=1, show_default=True, metavar="N", type=int,
     help="Number of processes to use")
-@click.option("--quantile",
+@click.option("-q", "--quantile",
     type=click.Choice(["2.5", "50", "97.5"]), default="50", show_default=True,
     help="""Filtering quantile to choose.""")
-@click.option("--refracs", "refracs_path",
-    type=click.Path(path_type=pathlib.Path, dir_okay=False, readable=True), required=True,
+@click.option("-r", "--refracs", "refracs_path",
+    type=click.Path(path_type=pathlib.Path, dir_okay=False, readable=True),
     help="""CSV file with population-specific refractive index choices.""")
 @click.argument("files", nargs=-1, type=click.Path(exists=True))
 def curate_vct_cmd(dbpath, outdir, process_count, quantile, refracs_path, files):
@@ -37,36 +37,27 @@ def curate_vct_cmd(dbpath, outdir, process_count, quantile, refracs_path, files)
     if files:
         files = util.expand_file_list(files)
         vct_paths = sorted([pathlib.Path(f) for f in files if f.endswith(".vct.parquet")])
-        refracs = pd.read_csv(refracs_path)
-        outliers = db.get_outliers_with_dates(dbpath)
-        ignore_dates = outliers[outliers["flag"] != 0]["date"].to_list()
+        if refracs_path:
+            refracs = pd.read_csv(refracs_path)
+        else:
+            refracs = None
+        if dbpath:
+            outliers = db.get_outliers_with_dates(dbpath)
+            ignore_dates = outliers[outliers["flag"] != 0]["date"].to_list()
+        else:
+            ignore_dates = None
         parallel = Parallel(return_as="generator_unordered", n_jobs=max(1, process_count))
         with tqdm(desc="files", total=len(files)) as bar:
-            for res in parallel(delayed(curate_vct_file)(f, refracs, quantile, ignore_dates, outdir) for f in vct_paths):
+            for res in parallel(delayed(curate_vct_file)(f, quantile, refracs, ignore_dates, outdir) for f in vct_paths):
                 bar.update(1)
                 bar.set_description(res)
 
 def curate_vct_file(
-    vct_path: pathlib.Path, refracs: pd.DataFrame, quantile: str,
-    ignore_dates: list[pd.Timestamp], outdir: pathlib.Path
+    vct_path: pathlib.Path, quantile: str, refracs: pd.DataFrame | None, 
+    ignore_dates: list[pd.Timestamp] | None, outdir: pathlib.Path
 ):
-    cols = [
-        "date",
-        f"q{quantile}",
-        f"pop_q{quantile}",
-        "fsc_small",
-        "chl_small",
-        "pe",
-        f"diam_lwr_q{quantile}",
-        f"diam_mid_q{quantile}",
-        f"diam_upr_q{quantile}",
-        f"Qc_lwr_q{quantile}",
-        f"Qc_mid_q{quantile}",
-        f"Qc_upr_q{quantile}"
-    ]
-    df = pd.read_parquet(vct_path, columns=cols)
-    df = vct.curate(df, refracs, quantile, ignore_dates)
+    df = vct.curate(vct_path, quantile, refracs, ignore_dates)
     if len(df):
         outdir.mkdir(parents=True, exist_ok=True)
-        df.to_parquet(outdir / vct_path.name)
+        df.to_parquet(outdir / vct_path.name, index = False)
     return vct_path.name
--- src/seaflowpy/vct.py
+++ src/seaflowpy/vct.py
@@ -2,11 +2,13 @@ from collections.abc import (
     Mapping,
     Sequence
 )
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
 def curate(
-    vct: pd.DataFrame, refracs: pd.DataFrame, quantile: str,
+    vct: pd.DataFrame | str | Path, quantile: str, refracs: pd.DataFrame | None=None, 
     ignore_dates: pd.Series | pd.DataFrame | Sequence | Mapping | None=None
 ):
     """Select VCT data for one quantile, population refractive indices, and remove ignored dates
@@ -15,18 +17,36 @@ def curate(
     """
     pop_col = f"pop_q{quantile}"
     quant_col = f"q{quantile}"
+    cols = ["date", "fsc_small", "chl_small", "pe", pop_col, quant_col]
+    if refracs is not None:
+        cols = cols + [
+            f"diam_lwr_{quant_col}",
+            f"diam_mid_{quant_col}",
+            f"diam_upr_{quant_col}",
+            f"Qc_lwr_{quant_col}",
+            f"Qc_mid_{quant_col}",
+            f"Qc_upr_{quant_col}"
+        ]
+    if not isinstance(vct, pd.DataFrame):
+        vct = pd.read_parquet(vct, columns = cols)
+    else:
+        vct = vct.copy()
+    vct = vct.rename(columns={pop_col: "pop"})
     vct = vct[vct[quant_col]]
     if ignore_dates:
         vct = vct.loc[~vct["date"].isin(ignore_dates)]
-    vct = vct.rename(columns={pop_col: "pop"}).copy()
-    vct["diam"] = np.NaN
-    vct["Qc"] = np.NaN
-    for pop in [c for c in refracs.columns if c != "cruise"]:
-        idx = vct["pop"] == pop
-        if idx.any():
-            vct.loc[idx, "diam"] = vct.loc[idx, f"diam_{refracs[pop].values[0]}_q{quantile}"]
-            vct.loc[idx, "Qc"] = vct.loc[idx, f"Qc_{refracs[pop].values[0]}_q{quantile}"]
-    return vct.reindex(columns=["date", "pop", "fsc_small", "chl_small", "pe", "diam", "Qc"])
+    if refracs is not None:
+        vct["diam"] = np.NaN
+        vct["Qc"] = np.NaN
+        for pop in [c for c in refracs.columns if c != "cruise"]:
+            idx = vct["pop"] == pop
+            if idx.any():
+                vct.loc[idx, "diam"] = vct.loc[idx, f"diam_{refracs[pop].values[0]}_q{quantile}"]
+                vct.loc[idx, "Qc"] = vct.loc[idx, f"Qc_{refracs[pop].values[0]}_q{quantile}"]
+        cols  = ["date", "pop", "fsc_small", "chl_small", "pe", "diam", "Qc"]
+    else:
+        cols  = ["date", "pop", "fsc_small", "chl_small", "pe"]
+    return vct.reindex(columns=cols)
 
 
 # def grid_vct(vct: pd.DataFrame, grid_bins: pd.DataFrame) -> pd.DataFrame:
@@ -40,3 +60,4 @@ def curate(
 #     counts["Qc_sum"] = gb.agg({"Qc": "sum"})["Qc"]
 #     counts = counts.reset_index()[group_cols + ["n", "Qc_sum"]]
 #     return counts
+
