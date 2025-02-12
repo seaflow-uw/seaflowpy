f73f958d92a36f46c535c3cde96c32eae8e00d27
Chris T. Berthiaume <chrisbee@uw.edu>
2025-02-12 12:30:50 -0800

    Switch from poetry to uv, pylint to ruff

--- build-docker.sh
+++ build-docker.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-poetry export --with=test >build-requirements.txt
-POETRY_APP_VERSION=$(poetry version | awk '{print $2}')
-poetry build
-echo "Building Docker image for ${POETRY_APP_VERSION} with Docker tag ${POETRY_APP_VERSION}"
-docker build --build-arg "ARG_APP_VERSION=${POETRY_APP_VERSION}" -t "ctberthiaume/seaflowpy:${POETRY_APP_VERSION}" .
+uv export --no-dev --all-extras --group test --no-emit-project >build-requirements.txt
+APP_VERSION=$(uv run seaflowpy version)
+if [[ -z "$APP_VERSION" ]]; then
+    echo "could not get seaflowpy version string" >&2
+    exit 1
+fi
+uv build
+echo "Building Docker image for ${APP_VERSION} with Docker tag ${APP_VERSION}"
+docker build --build-arg "ARG_APP_VERSION=${APP_VERSION}" -t "ctberthiaume/seaflowpy:${APP_VERSION}" .
 echo "Docker build complete, running seaflowpy tests in side container"
-docker run -it --rm "ctberthiaume/seaflowpy:${POETRY_APP_VERSION}" bash -c 'cd /seaflowpy-src/* && pytest'
+docker run -it --rm "ctberthiaume/seaflowpy:${APP_VERSION}" bash -c 'cd /seaflowpy-src/* && pytest'
